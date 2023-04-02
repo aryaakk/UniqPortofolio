5ed9d09c57baf7d59c79b7faed3e9ee21ef5797a
aryaakk <aryatiowasesa1306@gmail.com>
2023-04-02 20:11:36 +0700

    commit materi course is done

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,12 @@
 const board = document.querySelector(".board");
 
 const randomPosition = () => {
-  return ~~(Math.random() * 30) + 1;
+  return ~~(Math.random() * 15) + 1;
 };
 
 let config = {
   speed: 100,
+  level: 1,
   player: {
     x: randomPosition(),
     y: randomPosition(),
@@ -18,6 +19,17 @@ let config = {
     x: 0,
     y: 0,
   },
+  showTitle: function () {
+    const title = document.getElementById("title_1");
+    title.style.opacity = 1;
+    title.style.visibility = "visible";
+    title.style.zIndex = 1;
+    setTimeout(() => {
+      title.style.opacity = 0;
+      title.style.visibility = "hidden";
+      title.style.zIndex = -1;
+    }, 2000);
+  },
 };
 
 const games = {
@@ -25,14 +37,31 @@ const games = {
     board.innerHTML = `<div class="food" style="grid-area : ${config.food.y} / ${config.food.x}"></div>`;
   },
   createPlayer: function () {
-    board.innerHTML += `<div class="player" style="grid-area : ${config.player.y} / ${config.player.x}"></div>`;
+    board.innerHTML += `<div class="player" id="player" style="grid-area : ${config.player.y} / ${config.player.x}"></div>`;
   },
   movePLayer: function () {
     config.player.x += config.velocity.x;
     config.player.y += config.velocity.y;
   },
+  resetPlayerPosition: function () {
+    if (
+      config.player.x <= 0 ||
+      config.player.x >= 16 ||
+      config.player.y <= 0 ||
+      config.player.y >= 16
+    ) {
+      console.log("Mentok Bruh");
+      config.player.x = 7;
+      config.player.y = 7;
+    }
+  },
+  levelUp: function () {
+    config.level += 1;
+  },
   isWin: function () {
     if (config.food.x == config.player.x && config.food.y == config.player.y) {
+      this.levelUp();
+      config.showTitle();
       return true;
     } else {
       return false;
@@ -65,10 +94,27 @@ function movement(listen) {
   }
 }
 
+function movementPlayerHead() {
+  const player = document.getElementById("player");
+
+  if (config.velocity.x == 1) {
+    player.style.transform = "scaleX(-1)";
+  }
+  if (config.velocity.y == 1) {
+    player.style.transform = "rotate(-90deg)";
+  }
+  if (config.velocity.y == -1) {
+    player.style.transform = "rotate(90deg)";
+  }
+}
+
 function start() {
   games.createFood();
   games.createPlayer();
   games.movePLayer();
+  movementPlayerHead();
+  games.resetPlayerPosition();
+
   const wins = games.isWin();
   if (wins) {
     games.randomFood();
